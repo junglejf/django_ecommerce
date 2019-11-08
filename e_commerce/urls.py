31e8a8bbd56bb1c9d2959c861d51646d65366856
junglejf <jfcchagas@id.uff.br>
2019-11-08 02:11:35 -0300

    finalizando teste com static_files

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -13,13 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib import admin
 from django.urls import path
 from .views import home_page, about_page, contact_page, blog_page, courses_page, login_page, register_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', about_page),
+    path('blog/', blog_page),
     path('cursos/', courses_page),
     path('about/', about_page),
     path('contact/', contact_page),
@@ -27,3 +31,7 @@ urlpatterns = [
     path('register/', register_page),
     path('', home_page),
 ]
+
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
