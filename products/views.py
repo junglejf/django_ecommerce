db098f9638a41482a1af42b7c977404dda9218b1
junglejf <jfcchagas@id.uff.br>
2019-11-09 01:04:40 -0300

    import detail view e product_detail_view em views.py de project

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import ListView, DetailView
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Product
 
@@ -27,15 +27,18 @@ class ProductDetailView(DetailView):
     queryset = Product.objects.all()
     template_name = "products/detail.html"
     
-    #def get_context_data(self, *args, **kwargs):
-        #context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
-        #print(context)
-        #return context
+    def get_context_data(self):
+        context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
+        print(context)
+        return context
 
 #Function Based View
-def product_detail_view(request):
+def product_detail_view(request, pk= None, *args, **kwargs):
+    print(args)
+    print(kwargs)
+    instance = get_object_or_404(Product, pk = pk)
     queryset = Product.objects.all()
     context = {
-        'object_list': queryset
+        'object': instance
     }
     return render(request, "products/detail.html", context)
