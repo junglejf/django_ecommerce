a2e8e888495214bfedf7a2379ec6d93cb617e61f
junglejf <jfcchagas@id.uff.br>
2019-11-11 14:52:58 -0300

    create product custom manager

--- products/models.py
+++ products/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class ProductManager(models.Manager):
+    def get_by_id(self,id):
+        qs = self.get_queryse().filter(id = id)
+        if qs.count()==1:
+            return qs.first()
+        return None
 class Product(models.Model): #product_category
     title       = models.CharField(max_length=120)
     description = models.TextField()
