0dbc24b23a75998e7b1fa101998d3c3ed9001bb3
junglejf <jfcchagas@id.uff.br>
2019-11-27 17:27:40 -0300

    criado a view de logout

--- e_commerce/views.py
+++ e_commerce/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, get_user_model
+from django.contrib.auth import authenticate, login,logout, get_user_model
 from .forms import ContactForm, LoginForm, RegisterForm
 
 
@@ -76,6 +76,10 @@ def login_page(request):
             #Retorna uma mensagem de erro de 'invalid login'.
             print("Login inválido")
     return render(request, "auth/login.html", context)
+    
+def logout_page(request):
+    logout(request)
+    return render(request, "auth/logout.html", {})
 
 User = get_user_model()
 def register_page(request):
