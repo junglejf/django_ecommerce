d67a634ac9c28daeacf16e835620d94fc8e740b8
junglejf <jfcchagas@id.uff.br>
2019-12-06 11:43:16 -0300

    adicionando foreignkey produto-categoria

--- products/admin.py
+++ products/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Product # equivalente a from product.models import Product
+from .models import Product, Category # equivalente a from product.models import Product
+
 class ProductAdmin(admin.ModelAdmin):
 	list_display = ('__str__', 'slug')
 	class meta:
 		model = Product
 
+class CategoryAdmin(admin.ModelAdmin):
+	list_display = ('__str__', 'slug')
+	class meta:
+		model = Category
+
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
--- products/models.py
+++ products/models.py
@@ -4,6 +4,54 @@ from django.db.models.signals import pre_save
 from django.urls import reverse
 # Create your models here.
 
+
+
+class CategoryManager(models.Manager):
+    def get_queryset(self):
+        return CategoryQuerySet(self.model, using=self._db)
+
+    def all(self):
+        return self.get_queryset().active()
+
+    #def featured(self):
+        #return self.get_queryset().filter(featured = True)
+        #return self.get_queryset().featured()
+
+    def get_by_id(self,id):
+        qs = self.get_queryse().filter(id = id)
+        if qs.count()==1:
+            return qs.first()
+        return None
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True, unique=True)
+
+    objects = CategoryManager()
+    
+    def get_absolute_url(self):
+        #return "/products/{slug}/".format(slug = self.slug)
+        return reverse("products:list_products_product", kwargs={"slug":self.slug})
+    
+    #exibir o nome do produto
+    def __str__(self):
+        return self.name
+
+
+
+class CategoryQuerySet(models.query.QuerySet):
+    def active(self):
+        return self.filter(active=True)
+
+    def featured(self):
+        return self.filter(featured=True, active=True)
+
+def category_pre_save_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = unique_slug_generator(instance)
+
+pre_save.connect(category_pre_save_receiver, sender = Category)
+
 class ProductManager(models.Manager):
     def get_queryset(self):
         return ProductQuerySet(self.model, using=self._db)
@@ -19,8 +67,8 @@ class ProductManager(models.Manager):
         qs = self.get_queryse().filter(id = id)
         if qs.count()==1:
             return qs.first()
-    
         return None
+
 class Product(models.Model): #product_category
     title       = models.CharField(max_length=120)
     description = models.TextField()
@@ -29,7 +77,8 @@ class Product(models.Model): #product_category
     featured    = models.BooleanField(default = False)
     active      = models.BooleanField(default = True)
     slug        = models.SlugField(blank = True, unique = True)
-    
+
+
     objects = ProductManager()      
     
     def get_absolute_url(self):
@@ -52,3 +101,4 @@ def product_pre_save_receiver(sender, instance, *args, **kwargs):
         instance.slug = unique_slug_generator(instance)
 
 pre_save.connect(product_pre_save_receiver, sender = Product)
+
