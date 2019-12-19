67b18b1d78d0315ee5b407818a9c53085949d9b3
junglejf <jfcchagas@id.uff.br>
2019-12-18 21:31:47 -0300

    carrinho ajax removendo um item sem deixar o botão remover

--- carrinho/views.py
+++ carrinho/views.py
@@ -8,6 +8,16 @@ from carrinho.forms import QuantidadeForm, RemoveProdutoDoCarrinhoForm
 from products.models import Product
 from .models import Cart
 
+def cart_detail_api_view(request):
+    cart_obj, new_obj = Cart.objects.new_or_get(request)
+    products = [{"name": x.name,"price":x.price} for x in cart_obj.products.all()]
+    #product_list = []
+    #for x in cart_obj.products.all():
+        #product_list.append({"name":x.name, "price":x.price})
+
+    cart_data = {"products":products, "subtotal":cart_obj.subtotal, "total":cart_obj.total}
+    return JsonResponse (cart_data)
+
 def cart_home(request):
     cart_obj, new_obj= Cart.objects.new_or_get(request)
 
@@ -16,7 +26,6 @@ def cart_home(request):
 def cart_update(request):
     
     product_id = request.POST.get('product_id')
-
     if product_id is not None:
         try:
             product_obj = Product.objects.get(id=product_id)
@@ -27,14 +36,18 @@ def cart_update(request):
 
         if product_obj in cart_obj.products.all():
             cart_obj.products.remove(product_obj)
+            added = False
         else:
             cart_obj.products.add(product_obj) 
-        
+            added = True
+        request.session['cart_items'] = cart_obj.products.count()
+ 
         if request.is_ajax():
             print("request AJAX")
             json_data = {
                 "added": added,
                 "removed": not added,
+                "cartItemCount": cart_obj.products.count()
             }
             return JsonResponse(json_data)
     #cart_obj.products.add(product_id)
